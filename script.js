ece48daecf16c42a8b04681430b3a8a8f60ef457
Minestorm01 <112302130+Minestorm01@users.noreply.github.com>
2024-04-22 22:17:49 +1000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let currentSlide = 0;
-const slider = document.getElementById('slider');
-const slides = document.querySelectorAll('.slide');
-
-function moveSlide(direction) {
-    currentSlide = (currentSlide + direction
