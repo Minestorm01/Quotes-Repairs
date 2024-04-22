77fb2c07d25fc0294fb7007a3e1943cc13fbf6b0
Minestorm01 <112302130+Minestorm01@users.noreply.github.com>
2024-04-22 21:48:38 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const slider = document.getElementById('slider');
 const totalSlides = document.querySelectorAll('.slide').length;
 
 document.getElementById('next').addEventListener('click', function() {
-    currentSlide = (currentSlide + 1) % totalSlides;  // Ensure we loop around the slides
+    currentSlide = (currentSlide + 1) % totalSlides;
     const offset = -currentSlide * 100;  // This should be 100 times the current slide index
     slider.style.transform = `translateX(${offset}vw)`;
 });
